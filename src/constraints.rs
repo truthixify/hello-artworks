930b02044a4d42f3c16aae03d4c7e0e22a3948d8
truthixify <truthixify@gmail.com>
2025-02-04 21:09:56 +0100

    cleaned it up a bit more

--- src/constraints.rs
+++ src/constraints.rs
@@ -1,8 +1,5 @@
 use ark_ff::Field;
-use ark_relations::{
-    lc,
-    r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError},
-};
+use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError};
 
 #[derive(Clone)]
 pub struct MultiplierCircuit<F: Field> {
@@ -12,8 +9,11 @@ pub struct MultiplierCircuit<F: Field> {
 
 impl<F: Field> ConstraintSynthesizer<F> for MultiplierCircuit<F> {
     fn generate_constraints(self, cs: ConstraintSystemRef<F>) -> Result<(), SynthesisError> {
+        // Input variable
         let a = cs.new_witness_variable(|| self.a.ok_or(SynthesisError::AssignmentMissing))?;
         let b = cs.new_witness_variable(|| self.b.ok_or(SynthesisError::AssignmentMissing))?;
+
+        // Output variable
         let c = cs.new_witness_variable(|| {
             let a = self.a.ok_or(SynthesisError::AssignmentMissing)?;
             let b = self.b.ok_or(SynthesisError::AssignmentMissing)?;
@@ -21,8 +21,7 @@ impl<F: Field> ConstraintSynthesizer<F> for MultiplierCircuit<F> {
             Ok(a * b)
         })?;
 
-        cs.enforce_constraint(lc!() + a, lc!() + b, lc!() + c)?;
-        cs.enforce_constraint(lc!(), lc!(), lc!())?;
+        cs.enforce_constraint(a.into(), b.into(), c.into())?;
 
         Ok(())
     }
@@ -42,24 +41,24 @@ mod tests {
 
         println!("Creating proofs...");
 
-        let c = MultiplierCircuit::<Fr> {
+        let circuit = MultiplierCircuit::<Fr> {
             a: Some(Fr::from(2u64)),
             b: Some(Fr::from(3u64)),
         };
 
-        let v = Fr::from(2u64) * Fr::from(3u64); // c.a.unwrap() * c.b.unwrap();
+        let c = Fr::from(2u64) * Fr::from(3u64); // c.a.unwrap() * c.b.unwrap();
 
         println!("Setting up circuit...");
 
-        let (pk, vk) = Groth16::<Bls12_381>::circuit_specific_setup(c.clone(), rng).unwrap();
+        let (pk, vk) = Groth16::<Bls12_381>::circuit_specific_setup(circuit.clone(), rng).unwrap();
 
         println!("Proving...");
-        let proof = Groth16::<Bls12_381>::prove(&pk, c, rng).unwrap();
+        let proof = Groth16::<Bls12_381>::prove(&pk, circuit, rng).unwrap();
 
         println!("Processing verifier key...");
         let pvk = Groth16::<Bls12_381>::process_vk(&vk).unwrap();
 
         println!("Verifying...");
-        assert!(Groth16::<Bls12_381>::verify_with_processed_vk(&pvk, &vec![v], &proof).unwrap());
+        assert!(Groth16::<Bls12_381>::verify_with_processed_vk(&pvk, &vec![c], &proof).unwrap());
     }
 }
